43df3552756980f191148bde2869f51f14714f42
Dima Polo <prolonchuk5@gmail.com>
2023-08-26 18:42:08 +0300

    wrap context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/main.scss'
 import { AuthContextProvider } from './context/AuthContext'
+import { ChatContextProvider } from './context/ChatContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <AuthContextProvider>
-    <App />
+    <ChatContextProvider>
+      <App />
+    </ChatContextProvider>
   </AuthContextProvider>
 
   // </React.StrictMode>,
