ba3ea47e7ed52a2dc28e604c8d9f4d2d0d12be1c
Dima Polo <prolonchuk5@gmail.com>
2023-08-26 14:55:00 +0300

    firebase service conv list support

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
-import { doc, getFirestore, setDoc } from 'firebase/firestore'
+import { collection, doc, getDocs, getFirestore, onSnapshot, query, setDoc, where } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBwjZHFtcreNoCbxayQrb9fOhB38J8AiNI',
@@ -18,7 +18,20 @@ export const auth = getAuth()
 export const storage = getStorage()
 export const db = getFirestore()
 
-function getUser(username) {}
+export async function getUserByName(username) {
+  const q = query(collection(db, 'users'), where('displayName', '==', username))
+
+  const querySnapshot = await getDocs(q)
+
+  if (querySnapshot.empty) {
+    // Handle the scenario where no user matches the given username.
+    console.log('No user found with the given username.')
+    throw 'No user found with the given username.'
+  }
+  const userDoc = querySnapshot.docs[0].data()
+
+  return userDoc
+}
 
 export async function register(email, password, displayName, file) {
   const res = await createUserWithEmailAndPassword(auth, email, password)
@@ -53,4 +66,24 @@ export async function register(email, password, displayName, file) {
 }
 export async function login(email, password) {
   return await signInWithEmailAndPassword(auth, email, password)
+}
+
+export async function getUserConversations(userId) {
+  return new Promise((resolve, reject) => {
+    const unsub = onSnapshot(
+      doc(db, 'userConversations', userId),
+      (docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const data = Object.entries(docSnapshot.data()).map((conv) => ({ convId: conv[0], ...conv[1] }))
+          resolve({ data, unsub })
+        } else {
+          reject(new Error('No user data found'))
+        }
+        // unsub() // unsubscribe from the listener once we've got the data
+      },
+      (error) => {
+        reject(error)
+      }
+    )
+  })
 }
