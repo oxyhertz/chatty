72bfa08bddaecf80f2d99a3d74ff269d75c6cfb5
Dima Polo <prolonchuk5@gmail.com>
2023-08-28 02:11:24 +0300

    scroll to bottom msg

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { AuthContext } from '../context/AuthContext'
 export const MessagePreview = ({ msg }) => {
-  console.log('🚀 ~ file: MessagePreview.jsx:4 ~ MessagePreview ~ msg:', msg)
   const { loggedInUser } = useContext(AuthContext)
-  console.log('🚀 ~ file: MessagePreview.jsx:5 ~ MessagePreview ~ loggedInUser:', loggedInUser)
+  const msgPreviewRef = useRef()
+  useEffect(() => {
+    msgPreviewRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [message])
+
   return (
-    <div className={`msg-preview ${loggedInUser.uid !== msg.senderId ? 'to-user-msg' : ''}`}>
+    <div ref={msgPreviewRef} className={`msg-preview ${loggedInUser.uid !== msg.senderId ? 'to-user-msg' : ''}`}>
       <span>{msg.text}</span>
     </div>
   )
