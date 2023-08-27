b6518cf0cedfeb066826e4c2c3bd88cf62f072de
Dima Polo <prolonchuk5@gmail.com>
2023-08-28 02:08:02 +0300

    display user on img load + export addConv to fire

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,24 @@
 import React, { useContext, useState } from 'react'
-import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc } from 'firebase/firestore'
-import { db, getUserByName } from '../services/firebase'
+import { getUserByName, addConv } from '../services/firebase'
 import { AuthContext } from '../context/AuthContext'
 import { SearchIcon } from '@heroicons/react/outline'
+import OutsideClickHandler from 'react-outside-click-handler'
 export const Search = () => {
   const [username, setUsername] = useState('')
   const [user, setSearchedUser] = useState(null)
   const [err, setErr] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   const { loggedInUser } = useContext(AuthContext)
 
   const handleSearch = async () => {
+    if (err) setErr(false)
     try {
       const user = await getUserByName(username)
-      console.log('🚀 ~ file: Search.jsx:15 ~ handleSearch ~ user:', user)
       setSearchedUser(user)
     } catch (err) {
-      console.log('🚀 ~ file: Search.jsx:18 ~ handleSearch ~ err:', err)
       setErr(true)
+      setSearchedUser(null)
     }
   }
 
@@ -29,33 +30,9 @@ export const Search = () => {
     //check whether the group(chats in firestore) exists, if not create
     const combinedId = loggedInUser.uid > user.uid ? loggedInUser.uid + user.uid : user.uid + loggedInUser.uid
     try {
-      const res = await getDoc(doc(db, 'conversations', combinedId))
-
-      if (!res.exists()) {
-        //create a chat in chats collection
-        await setDoc(doc(db, 'conversations', combinedId), { messages: [] })
-
-        //create user chats
-        await updateDoc(doc(db, 'userConversations', loggedInUser.uid), {
-          [combinedId + '.userInfo']: {
-            uid: user.uid,
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-          },
-          [combinedId + '.date']: serverTimestamp(),
-        })
-
-        await updateDoc(doc(db, 'userConversations', user.uid), {
-          [combinedId + '.userInfo']: {
-            uid: loggedInUser.uid,
-            displayName: loggedInUser.displayName,
-            photoURL: loggedInUser.photoURL,
-          },
-          [combinedId + '.date']: serverTimestamp(),
-        })
-      }
+      await addConv(combinedId, loggedInUser, user)
     } catch (err) {
-      console.log('🚀 ~ file: Search.jsx:59 ~ handleSelect ~ err:', err)
+      setErr(true)
     }
     setSearchedUser(null)
     setUsername('')
@@ -66,14 +43,23 @@ export const Search = () => {
         <SearchIcon />
         <input type="text" placeholder="Find a user" onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} value={username} />
       </div>
-      {err && <span>User not found!</span>}
+      {err && <div>User not found!</div>}
       {user && (
-        <div className="userChat" onClick={handleSelect}>
-          <img src={user.photoURL} alt="" />
-          <div className="userChatInfo">
-            <span>{user.displayName}</span>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setSearchedUser(null)
+            setUsername('')
+          }}
+        >
+          <div style={{ display: isImageLoaded ? '' : 'none' }} className="searched-user" onClick={handleSelect}>
+            <img src={user.photoURL} alt="" onLoad={() => setIsImageLoaded(true)} className="rounded-img" />
+            {isImageLoaded && (
+              <div className="searched-user-nfo">
+                <span>{user.displayName}</span>
+              </div>
+            )}
           </div>
-        </div>
+        </OutsideClickHandler>
       )}
     </div>
   )
